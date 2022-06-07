5e2145765194b90885a2fa4dffc1bb22b8cd8d58
SagarBhivara <sagarbhivara@gmail.com>
2022-06-07 17:05:30 +0530

    third commit

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-const express = require('express')
-const cors = require('cors')
-const https = require('https')
-const app = express()
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const PORT = process.env.PORT || 4000;
-const requrl="https://jsonplaceholder.typicode.com/users";
 
+const options ={
+    definition: {
+        info:{
+            openapi: "0.0.1",
+            title: "Task API",
+            description: "API Integration in Swagger UI",
+            contact:{
+                name:"Sagar Bhivara",
+                email:"sagarbhivara@gmail.com"
+            },
+            servers:[
+                {
+                    url:"https://localhost:4000"
+                }
+            ]
+        }
+    },
+    apis: ["index.js"]
+};
 
-https.get(requrl, (resp) => {
-  let data = '';
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));   
 
-  
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+// Routes
+/** 
+ * @swagger
+ * /users:
+ *  get:
+ *    description: Use to request all users
+ *    responses:
+ *      '200':
+ *       description: A Successful Response
+*/  
+app.get('/users',(req,res)=>{
+    res.send('users result')
 });
-app.listen(PORT, ()=>{console.log(`The Server is running on port ${PORT}`)})
+
+app.listen(PORT, ()=>{console.log(`The Server is listening on port ${PORT}`)})
--- routes/user-route.js
+++
@@ -1,7 +0,0 @@
-// const express = require ('express')
-// const router = require ('router')
-
-// router.get("/", (req,res)=>{
-//     const users = req.app.udb.get("users")
-//     res.send(users)
-// })
